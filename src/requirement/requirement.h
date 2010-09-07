27b71a1cec24e0479a733149365f11ff87451174
agudpp@gmail.com <agudpp@gmail.com@78c94162-ff61-52e8-3269-482ca6dd2f2f>
2010-09-07 22:23:53 +0000

    modifying the class diagram and creating other classes

---
+++ src/requirement/requirement.h
@@ -0,0 +1,70 @@
+/*! This is the abstraction of a requirement. It can be linked to functions or 
+ * modules (if this functions or modules are fully completed, then we can say 
+ * that the requirement it is covered) 
+*/
+#ifndef REQUIREMENT_H
+#define REQUIREMENT_H
+
+#include <string>
+#include <list>
+#include <iostream>
+#include <stdio.h>
+/* own libs */
+#include "moodule.h"
+#include "function.h"
+
+
+class Requirement {
+	
+	public:
+		/* empty consturctor */
+		Requirement(void);
+		
+		/* Function to set the description of the requirement
+		 * REQUIRES:
+		 * 	desc.length() > 0 
+		 */
+		void setDescription(string &desc);
+		
+		/* Function to set the title of the requirement
+		 * REQUIRES:
+		 * 	title.length() > 0 
+		 */
+		void setTitle(string &title);
+		
+		/* Set and get the status */
+		int getStatus(void){return this->status;};
+		void setStatus(int st){this->status = st;};
+		
+		/* Function to add a function who the requirements depends of
+		 * REQUIRES:
+		 * 	func	!= NULL
+		 */
+		void addFuncDependency(Function *func);
+		
+		/* Function to add a module who the requirements depends of
+		 * REQUIRES:
+		 * 	mod	!= NULL
+		 */
+		void addModuleDependency(Module *mod);
+		
+		/* empty destructor */
+		~Requirement(void);
+	
+	private:
+		/* Requirement description */
+		string description;
+		/* Requirement title */
+		string title;
+		/* status of the requirement */
+		int status;
+		/* the list of the function dependencies */
+		list<Function *> funcDependenciesList;
+		/* the list of the modules dependencies */
+		list<Module *> moduleDependenciesList;
+		
+};
+
+
+#endif
+
