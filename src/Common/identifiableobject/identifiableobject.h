7103ef4c4a548769db0e6b00a8043ee070661e23
agudpp@gmail.com <agudpp@gmail.com@78c94162-ff61-52e8-3269-482ca6dd2f2f>
2011-04-24 23:31:45 +0000

    new version...

---
+++ src/Common/identifiableobject/identifiableobject.h
@@ -0,0 +1,34 @@
+#ifndef IDENTIFIABLEOBJECT_H
+#define IDENTIFIABLEOBJECT_H
+
+#include <string>
+
+using namespace std;
+
+/*! we gonna define the type used to the ID */
+typedef int	ObjectId_t;
+
+class IdentifiableObject {
+
+	// private
+	static int globalID = 0;
+
+	public:
+		/* constructor */
+		IdentifiableObject(){mID = ++globalID;} // TODO: not thread safe
+
+		ObjectId_t getID(void){return mID;};
+		
+		bool operator==(IdentifiableObject &other){return (mID == other.getID());}
+		
+		virtual ~IdentifiableObject(){};
+		
+	private:
+		ObjectId_t mID;
+	
+};
+
+
+
+
+#endif
