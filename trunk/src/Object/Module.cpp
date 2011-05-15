a83da21aa15045dee3e3e90f4bec063564e4ebe0
agudpp@gmail.com <agudpp@gmail.com@78c94162-ff61-52e8-3269-482ca6dd2f2f>
2011-05-15 03:11:13 +0000

    adding new diagrams! :)

--- trunk/src/Object/Module.cpp
+++ trunk/src/Object/Module.cpp
@@ -15,3 +15,44 @@ Module::Module():IObject(IObject::TYPE_MODULE) {
 Module::~Module() {
 	// TODO Auto-generated destructor stub
 }
+
+
+
+
+/* Add a new IObject. If the object already exits this function do nothing
+ * REQUIRES:
+ * 		obj		!= 0
+ */
+void Module::addNewObject(IObject *obj)
+{
+	ASSERT(obj);
+
+	if(mObjects.find(obj->getType()) != mObjects.end()){
+		return;
+	}
+
+	mObjects[obj->getType()].push_back(obj);
+}
+
+/* Remove a IObject
+ * REQUIRES:
+ * 		obj		!= 0
+ */
+void Module::removeObject(IObject *obj)
+{
+	ASSERT(obj);
+
+	if(mObjects.find(obj->getType()) == mObjects.end()){
+		return;
+	}
+
+	mObjects[obj->getType()].remove(obj);
+}
+
+/* Get a list of objects of a certain kind (or empty list if haven't) */
+const std::list<IObject*> &Module::getObjects(IObject::ObjectType_t t)
+{
+	// note that in this case the list is created if it not exists..
+	return mObjects[t];
+}
+
--- trunk/src/Object/Module.h
+++ trunk/src/Object/Module.h
@@ -9,14 +9,55 @@
 #define MODULE_H_
 
 #include <string>
+#include <list>
+#include <map>
 
 #include "DebugUtil.h"
 #include "IObject.h"
 
-class Module : public IObject{
+class Module : public IObject {
 public:
 	Module();
 	virtual ~Module();
+
+
+	/* Add a new IObject. If the object already exits this function do nothing
+	 * REQUIRES:
+	 * 		obj		!= 0
+	 */
+	void addNewObject(IObject *obj);
+
+	/* Remove a IObject
+	 * REQUIRES:
+	 * 		obj		!= 0
+	 */
+	void removeObject(IObject *obj);
+
+	/* Get a list of objects of a certain kind (or empty list if haven't) */
+	const std::list<IObject*> &getObjects(IObject::ObjectType_t t);
+
+
+
+	/* 			This will be the extra values used by the Module 			 */
+	/* 			Getters			/			Setters							 */
+
+	const std::string &getDescription() const {return mDescription;}
+	const std::string &getFileName() const {return mFileName;}
+	const std::string &getModuleName() const {return mModuleName;}
+	void setDescription(const std::string &d) {mDescription = d;}
+	void setFileName(const std::string &f) {mFileName = f;}
+	void setModuleName(const std::string &m) {mModuleName = m;}
+
+private:
+	// Description of the module
+	std::string mDescription;
+	// FileName
+	std::string mFileName;
+	// Module Name
+	std::string mModuleName;
+
+	// Objects associated to the module
+	std::map<IObject::ObjectType_t, std::list<IObject*> > mObjects;
 };
 
 #endif /* MODULE_H_ */
